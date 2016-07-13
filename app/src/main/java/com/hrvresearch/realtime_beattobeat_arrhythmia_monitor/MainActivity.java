8ec30fbc72ba9cdab7c1a2c3d00421143755c100
dostyvsky <lowell@schat.com>
2016-07-13 07:43:37 -0700

    adding xml files

--- app/src/main/java/com/hrvresearch/realtime_beattobeat_arrhythmia_monitor/MainActivity.java
+++ app/src/main/java/com/hrvresearch/realtime_beattobeat_arrhythmia_monitor/MainActivity.java
@@ -4,7 +4,8 @@ package com.hrvresearch.realtime_beattobeat_arrhythmia_monitor;
         import android.content.Context;
         import android.graphics.Color;
         import android.graphics.Paint.Align;
-        import android.os.Build;
+ import android.net.Uri;
+ import android.os.Build;
         import android.os.Bundle;
         import android.os.Environment;
         import android.provider.Settings;
@@ -17,7 +18,11 @@ package com.hrvresearch.realtime_beattobeat_arrhythmia_monitor;
         import android.widget.TextView;
 
         // For Graphing
-        import org.achartengine.ChartFactory;
+ import com.google.android.gms.appindexing.Action;
+ import com.google.android.gms.appindexing.AppIndex;
+ import com.google.android.gms.common.api.GoogleApiClient;
+
+ import org.achartengine.ChartFactory;
         import org.achartengine.GraphicalView;
         import org.achartengine.chart.PointStyle;
         import org.achartengine.model.TimeSeries;
@@ -36,9 +41,13 @@ public class MainActivity extends Activity {
 
     // graph max and min set for use by our elderly patient.
     // TODO make max and min heart rate user configurable
-    int MAX_HEART_RATE=110;
-    int MIN_HEART_RATE = 30;
+    // 110 is very low but the app was designed for elderly patients.
+    int MAX_HEART_RATE = 140;
+    int MIN_HEART_RATE = 40;
 
+    // user settings to be restored on
+    int prexistingRotation ;
+    int preexitingScreenSleep;
 
     // Settings you may wish to change regarding the coloring
     int linecolor = Color.YELLOW;
@@ -53,13 +62,17 @@ public class MainActivity extends Activity {
     private GraphicalView chartview;
 
 
-
     ZephyrManager zm;
     private long x;
     private int batterylevel;
     SelfClearingMessage status;
     String title = "heart rate";
     private Context context;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
 
     @Override
@@ -74,7 +87,7 @@ public class MainActivity extends Activity {
         getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         status = new SelfClearingMessage(this);
         // TODO make this setting of x  DRY in eliminating it multiple places
@@ -82,12 +95,23 @@ public class MainActivity extends Activity {
 
         // Set screen to landscape mode and try to keep device powered on
         Settings.System.putInt(this.getContentResolver(), Settings.System.ACCELEROMETER_ROTATION, 0); // turn off auto rotation
+
+        try {
+            prexistingRotation = Settings.System.getInt(this.getContentResolver(), Settings.System.USER_ROTATION);
+            preexitingScreenSleep = getWindow().getAttributes().flags;
+
+        } catch (Settings.SettingNotFoundException e) {
+
+
+
+            System.out.println("setting not found");
+        }
         Settings.System.putInt(this.getContentResolver(), Settings.System.USER_ROTATION, 1);  // 1 locks to david's NEW prefered orientation
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         context = getApplicationContext();
 
-       // for Achart engine
+        // for Achart engine
         series = new TimeSeries(title);
         eseries = new TimeSeries("Heart Efficiency");
         mDataset = new XYMultipleSeriesDataset();
@@ -103,6 +127,9 @@ public class MainActivity extends Activity {
                 updateChart((int) (Math.random() * 1000 + 500));
             }
         }
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     // the zephry manager will send RR values to this listener.
@@ -110,7 +137,7 @@ public class MainActivity extends Activity {
     public void updateChart(int rr) {
 
         // we throw out any 0s or absurd values.  We have found this tends not to happen with a good chest strap
-        if ((rr < 1 ) || (rr > 3000)) return;
+        if ((rr < 1) || (rr > 3000)) return;
         int pulserate = 60000 / rr;
         series.add(x, pulserate);
         x = x + rr;  // time is also the accumulation of RR values
@@ -138,7 +165,7 @@ public class MainActivity extends Activity {
         mRenderer.setShowGrid(true);
         mRenderer.setGridColor(gridcolor);
         mRenderer.setXLabels(11);
-        mRenderer.setYLabels(10);
+        mRenderer.setYLabels(20);
 
         mRenderer.setAxisTitleTextSize(16);
         mRenderer.setChartTitleTextSize(20);
@@ -160,12 +187,11 @@ public class MainActivity extends Activity {
 
         chartview = ChartFactory.getTimeChartView(context, mDataset, mRenderer, null);
         chartview.invalidate();
-        ((TextView)findViewById(R.id.heartratelabel)).setText("HR:" + pulserate);
-        ((TextView)findViewById(R.id.batterylabel)).setText("batt:" +  batterylevel);
+        ((TextView) findViewById(R.id.heartratelabel)).setText("HR:" + pulserate);
 
         chartview.repaint();
 
-        FrameLayout chartlayout = (FrameLayout)findViewById(R.id.chartlayout);
+        FrameLayout chartlayout = (FrameLayout) findViewById(R.id.chartlayout);
         chartlayout.removeAllViewsInLayout();
 
 
@@ -184,36 +210,37 @@ public class MainActivity extends Activity {
 
     public void displayBattery(int batterylevel) {
         this.batterylevel = batterylevel;
+        ((TextView) findViewById(R.id.batterylabel)).setText("batt:" + batterylevel);
     }
 
 
     // write RR values to a text file kept in the application data directory for possible later use
     public void writeRR(View v) {
-        try{
+        try {
 
-   // status.setMessage("writing RR values to file");
-    SimpleDateFormat format = new SimpleDateFormat("yyyy_MM_dd_HH_mm");
+            // status.setMessage("writing RR values to file");
+            SimpleDateFormat format = new SimpleDateFormat("yyyy_MM_dd_HH_mm");
 
-    // give user feedback the button worked
-    v.playSoundEffect(android.view.SoundEffectConstants.CLICK);
+            // give user feedback the button worked
+            v.playSoundEffect(SoundEffectConstants.CLICK);
 
-    double starttime = series.getX(0);
-    String mydate = (format.format(starttime));
+            double starttime = series.getX(0);
+            String mydate = (format.format(starttime));
 
-    String FILENAME = "rr_" + mydate + ".txt";
-    status.setMessage("writing RR values to file to Documents  " + FILENAME);
-    File path = Environment.getExternalStoragePublicDirectory(
-            Environment.DIRECTORY_DOCUMENTS);
-    File file = new File(path, "/" + FILENAME);
+            String FILENAME = "rr_" + mydate + ".txt";
+            status.setMessage("writing RR values to file to Documents  " + FILENAME);
+            File path = Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOCUMENTS);
+            File file = new File(path, "/" + FILENAME);
 
-    dbg(file.getAbsolutePath().toString());
-    file.createNewFile();
+            dbg(file.getAbsolutePath().toString());
+            file.createNewFile();
 
-        FileOutputStream fos = new FileOutputStream(file);
+            FileOutputStream fos = new FileOutputStream(file);
 
-            for (int i = 1;i<series.getItemCount();i++)
-            {int temprr = (int) (series.getX(i)-series.getX(i-1));
-            String s = String.valueOf(temprr) + "\n";
+            for (int i = 1; i < series.getItemCount(); i++) {
+                int temprr = (int) (series.getX(i) - series.getX(i - 1));
+                String s = String.valueOf(temprr) + "\n";
 
                 fos.write(s.getBytes());
             }
@@ -232,7 +259,7 @@ public class MainActivity extends Activity {
         x = System.currentTimeMillis();
         zm.connect();
 
-        }
+    }
 
     @Override
 
@@ -241,29 +268,75 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event)
-    {
-   // maybe this gets rid of any keys we don't want pressed
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // maybe this gets rid of any keys we don't want pressed
         dbg("keydown method called");
 
-return false;
+        return false;
     }
 
 
-   // protected void onStop() {
-   //     super.onStop();  // Always call the superclass method first
-// TODO have app reset screen orientation back to the way it was when app started
-
-
-        // Save the note's current draft, because the activity is stopping
-        // and we want to be sure the current note progress isn't lost.
-    //    ContentValues values = new ContentValues();
-  //      values.put(NotePad.Notes.COLUMN_NAME_TITLE, getCurrentNoteTitle());
-//values.putAll(
-
-   // }
-
     public void setTempText(String message) {
         ((TextView) findViewById(R.id.messages)).setText("    " + message);
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();  // Always call the superclass method first
+
+        Settings.System.putInt(this.getContentResolver(), Settings.System.USER_ROTATION, prexistingRotation);  // 1 locks to david's NEW prefered orientation
+
+//    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+}
+
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        Settings.System.putInt(this.getContentResolver(), Settings.System.USER_ROTATION, 1);  // 1 locks to david's NEW prefered orientation
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.hrvresearch.realtime_beattobeat_arrhythmia_monitor/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.hrvresearch.realtime_beattobeat_arrhythmia_monitor/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
